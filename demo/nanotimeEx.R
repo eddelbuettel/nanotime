a3f781360aa08128e4adef09da39a850ce3a9726
Dirk Eddelbuettel <edd@debian.org>
2016-12-16 18:58:07 -0600

    additional labels on plot

--- demo/nanotimeEx.R
+++ demo/nanotimeEx.R
@@ -3,7 +3,7 @@ library(nanotime)
 set.seed(42)
 N <- 300
 shine <- nanotime(Sys.time()) + cumsum(10*rpois(n=N+1, lambda=4))
-rain <- nanotime(Sys.time()) + cumsum(10*rpois(n=N+1, lambda=4) + round(runif(N+1)*50))
+rain <- nanotime(Sys.time()) + cumsum(10*rpois(n=N+1, lambda=4) + round(runif(N+1)*25))
 
 library(data.table)
 library(bit64)
@@ -20,4 +20,6 @@ ddf <- data.frame(val=as.numeric(c(diff(rain),diff(shine))), # need to cast to n
                     key=rep(c("rain", "shine"), each=N))
 head(df)
 library(ggplot2)
-ggplot(ddf, aes(key, val)) + geom_violin(aes(fill=key)) + coord_flip()
+ggplot(ddf, aes(key, val)) + geom_violin(aes(fill=key)) + coord_flip() + 
+    ylab("Message Time (in nanoseconds)") + xlab("Weather") + ggtitle("Nanosecond Delay", "Under Different Weather Conditions.")
+
