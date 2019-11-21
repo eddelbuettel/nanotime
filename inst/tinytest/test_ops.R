97616c27634516ca0b74dd62eb34b10209d81904
Dirk Eddelbuettel <edd@debian.org>
2019-11-21 08:02:49 -0600

    tinytest conversion step three: convert files

--- inst/tinytest/test_ops.R
+++ inst/tinytest/test_ops.R
@@ -1,120 +1,122 @@
+
+library(nanotime)
+
 ## ------------ `-`
-"test_nanotime-nanotime" <- function() {
-  checkEquals(nanotime(2)  - nanotime(1),  as.integer64(1))
-  checkEquals(nanotime(-1) - nanotime(-2), as.integer64(1))
-}
-"test_nanotime-integer64" <- function() {
-  checkEquals(nanotime(2)  - as.integer64(1),  nanotime(1))
-  checkEquals(nanotime(-1) - as.integer64(-2), nanotime(1))
-}
-"test_nanotime-integer" <- function() {
-  checkEquals(nanotime(2)  - 1L,  nanotime(1))
-  checkEquals(nanotime(-1) - -2L, nanotime(1))
-}
-"test_nanotime-character" <- function() {
-  checkException(nanotime(0) - "A", "invalid operand types")
-}
-"test_character-nanotime" <- function() {
-  checkException("A" - nanotime(0), "invalid operand types")
-}
-"test_numeric-nanotime" <- function() {
-  checkException(1 - nanotime(0), "invalid operand types")
-}
+#"test_nanotime-nanotime" <- function() {
+expect_equal(nanotime(2)  - nanotime(1),  as.integer64(1))
+expect_equal(nanotime(-1) - nanotime(-2), as.integer64(1))
+
+#"test_nanotime-integer64" <- function() {
+expect_equal(nanotime(2)  - as.integer64(1),  nanotime(1))
+expect_equal(nanotime(-1) - as.integer64(-2), nanotime(1))
+
+#"test_nanotime-integer" <- function() {
+expect_equal(nanotime(2)  - 1L,  nanotime(1))
+expect_equal(nanotime(-1) - -2L, nanotime(1))
+
+#"test_nanotime-character" <- function() {
+expect_error(nanotime(0) - "A", "invalid operand types")
+
+#"test_character-nanotime" <- function() {
+expect_error("A" - nanotime(0), "invalid operand types")
+
+#"test_numeric-nanotime" <- function() {
+expect_error(1 - nanotime(0), "invalid operand types")
+
 
 
 ## ----------- `+`
-"test_nanotime+numeric" <- function() {
-  checkEquals(nanotime(0) +  1, nanotime(1))
-  checkEquals(nanotime(0) + -1, nanotime(-1))
-}
+#"test_nanotime+numeric" <- function() {
+expect_equal(nanotime(0) +  1, nanotime(1))
+expect_equal(nanotime(0) + -1, nanotime(-1))
 
-"test_nanotime+integer" <- function() {
-  checkEquals(nanotime(0) +  1L, nanotime(1))
-  checkEquals(nanotime(0) + -1L, nanotime(-1))
-}
-"test_nanotime+integer64" <- function() {
-  checkEquals(nanotime(0) + as.integer64(1), nanotime(1))
-  checkEquals(nanotime(0) + as.integer64(-1), nanotime(-1))
-}
-"test_nanotime+character" <- function() {
-  checkException(nanotime(0) + "A", "invalid operand types")
-}
-"test_character+nanotime" <- function() {
-  checkException("A" + nanotime(0), "invalid operand types")
-}
-"test_nanotime+nanotime" <- function() {
-  checkException(nanotime(1) + nanotime(0), "invalid operand types")
-}
-"test_nanotime+integer64" <- function() {
-  checkEquals(nanotime(0) + as.integer64(1), nanotime(1))
-  checkEquals(nanotime(0) + as.integer64(-1), nanotime(-1))
-}
-"test_numeric+nanotime" <- function() {
-  checkEquals(nanotime(0) +  1, nanotime( 1))
-  checkEquals(nanotime(0) + -1, nanotime(-1))
-}
-"test_integer+nanotime" <- function() {
-  checkEquals( 1L + nanotime(0), nanotime(1))
-  checkEquals(-1L + nanotime(0), nanotime(-1))
-}
+
+#"test_nanotime+integer" <- function() {
+expect_equal(nanotime(0) +  1L, nanotime(1))
+expect_equal(nanotime(0) + -1L, nanotime(-1))
+
+#"test_nanotime+integer64" <- function() {
+expect_equal(nanotime(0) + as.integer64(1), nanotime(1))
+expect_equal(nanotime(0) + as.integer64(-1), nanotime(-1))
+
+#"test_nanotime+character" <- function() {
+expect_error(nanotime(0) + "A", "invalid operand types")
+
+#"test_character+nanotime" <- function() {
+expect_error("A" + nanotime(0), "invalid operand types")
+
+#"test_nanotime+nanotime" <- function() {
+expect_error(nanotime(1) + nanotime(0), "invalid operand types")
+
+#"test_nanotime+integer64" <- function() {
+expect_equal(nanotime(0) + as.integer64(1), nanotime(1))
+expect_equal(nanotime(0) + as.integer64(-1), nanotime(-1))
+
+#"test_numeric+nanotime" <- function() {
+expect_equal(nanotime(0) +  1, nanotime( 1))
+expect_equal(nanotime(0) + -1, nanotime(-1))
+
+#"test_integer+nanotime" <- function() {
+expect_equal( 1L + nanotime(0), nanotime(1))
+expect_equal(-1L + nanotime(0), nanotime(-1))
+
 
 
 ## ---------- other ops
-test_compare_nanotime_ANY <- function() {
-  checkTrue(nanotime(1) == nanotime(1))
-  checkTrue(nanotime(1) == as.integer64(1))
-  checkTrue(nanotime(1) == 1L)
-  checkTrue(nanotime(1) == 1)
-  checkException(is.na(nanotime(1) == "a"))  # now passes on and gets parse error
+#test_compare_nanotime_ANY <- function() {
+expect_true(nanotime(1) == nanotime(1))
+expect_true(nanotime(1) == as.integer64(1))
+expect_true(nanotime(1) == 1L)
+expect_true(nanotime(1) == 1)
+expect_error(is.na(nanotime(1) == "a"))  # now passes on and gets parse error
 
-  checkTrue(!(nanotime(1) < nanotime(1)))
-  checkTrue(!(nanotime(1) < as.integer64(1)))
-  checkTrue(!(nanotime(1) < 1L))
-  checkTrue(!(nanotime(1) < 1))
-}
-test_compare_ANY_nanotime <- function() {
-  checkTrue(as.integer64(1) == nanotime(1))
-  checkTrue(1L == nanotime(1))
-  checkTrue(1 == nanotime(1))
-  checkTrue(!("a" == is.na(nanotime(1))))
+expect_true(!(nanotime(1) < nanotime(1)))
+expect_true(!(nanotime(1) < as.integer64(1)))
+expect_true(!(nanotime(1) < 1L))
+expect_true(!(nanotime(1) < 1))
 
-  checkTrue(!(as.integer64(1) < nanotime(1)))
-  checkTrue(!(1L < nanotime(1)))
-  checkTrue(!(1 < nanotime(1)))
-}
-test_compare_character_nanotime <- function() {
-    checkTrue(isFALSE("2018-12-28T16:34:59.649943448+00:00" < nanotime("2018-12-28T16:34:59.000000000+00:00")))
-    checkTrue("2018-12-28T16:34:59.649943448+00:00" >  nanotime("2018-12-28T16:34:59.000000000+00:00"))
-    checkTrue("2018-12-28T16:34:59.649943448+00:00" == nanotime("2018-12-28T16:34:59.649943448+00:00"))
-}
-test_compare_nanotime_character <- function() {
-    checkTrue(isFALSE(nanotime("2018-12-28T16:34:59.649943448+00:00") < "2018-12-28T16:34:59.000000000+00:00"))
-    checkTrue(nanotime("2018-12-28T16:34:59.649943448+00:00") >  "2018-12-28T16:34:59.000000000+00:00")
-    checkTrue(nanotime("2018-12-28T16:34:59.649943448+00:00") == "2018-12-28T16:34:59.649943448+00:00")
-}
-test_Logic <- function() {
-  exc <- "operations are possible only for numeric, logical or complex types"
-  checkException(nanotime(1) & nanotime(1), exc)
-  checkException(nanotime(1) & as.integer64(1), exc)
-  checkException(nanotime(1) & TRUE, exc)
+#test_compare_ANY_nanotime <- function() {
+expect_true(as.integer64(1) == nanotime(1))
+expect_true(1L == nanotime(1))
+expect_true(1 == nanotime(1))
+expect_true(!("a" == is.na(nanotime(1))))
 
-  checkException(as.integer64(1) & nanotime(1), exc)
-  checkException(1L & nanotime(1), exc)
-  checkException(FALSE & nanotime(1), exc)
-}
-test_Math <- function() {
-  checkException(abs(nanotime(1)), "non-numeric argument to mathematical function")
-}
-test_Math2 <- function() {
-  checkException(round(nanotime(1), 2), "non-numeric argument to mathematical function")
-}
-test_Summary <- function() {
-  checkException(sum(nanotime(1)), "invalid 'type' (nanotime) of argument")
+expect_true(!(as.integer64(1) < nanotime(1)))
+expect_true(!(1L < nanotime(1)))
+expect_true(!(1 < nanotime(1)))
 
-  checkEquals(min(nanotime(c(1,8,4,2,0,3,10))), nanotime(0))
-  checkEquals(max(nanotime(c(1,8,4,2,0,3,10))), nanotime(10))
-  checkEquals(range(nanotime(c(1,8,4,2,0,3,10))), nanotime(c(0, 10)))
-}
-test_Complex <- function() {
-  checkException(Arg(nanotime(1)), "non-numeric argument to function")
-}
+#test_compare_character_nanotime <- function() {
+expect_true(isFALSE("2018-12-28T16:34:59.649943448+00:00" < nanotime("2018-12-28T16:34:59.000000000+00:00")))
+expect_true("2018-12-28T16:34:59.649943448+00:00" >  nanotime("2018-12-28T16:34:59.000000000+00:00"))
+expect_true("2018-12-28T16:34:59.649943448+00:00" == nanotime("2018-12-28T16:34:59.649943448+00:00"))
+
+#test_compare_nanotime_character <- function() {
+expect_true(isFALSE(nanotime("2018-12-28T16:34:59.649943448+00:00") < "2018-12-28T16:34:59.000000000+00:00"))
+expect_true(nanotime("2018-12-28T16:34:59.649943448+00:00") >  "2018-12-28T16:34:59.000000000+00:00")
+expect_true(nanotime("2018-12-28T16:34:59.649943448+00:00") == "2018-12-28T16:34:59.649943448+00:00")
+
+#test_Logic <- function() {
+exc <- "operations are possible only for numeric, logical or complex types"
+#expect_error(nanotime(1) & nanotime(1), exc)
+expect_error(nanotime(1) & as.integer64(1), exc)
+expect_error(nanotime(1) & TRUE, exc)
+
+expect_error(as.integer64(1) & nanotime(1), exc)
+expect_error(1L & nanotime(1), exc)
+expect_error(FALSE & nanotime(1), exc)
+
+#test_Math <- function() {
+expect_error(abs(nanotime(1)), "non-numeric argument to mathematical function")
+
+#test_Math2 <- function() {
+expect_error(round(nanotime(1), 2), "non-numeric argument to mathematical function")
+
+#test_Summary <- function() {
+expect_error(sum(nanotime(1)))#, "invalid 'type' (nanotime) of argument")
+
+expect_equal(min(nanotime(c(1,8,4,2,0,3,10))), nanotime(0))
+expect_equal(max(nanotime(c(1,8,4,2,0,3,10))), nanotime(10))
+expect_equal(range(nanotime(c(1,8,4,2,0,3,10))), nanotime(c(0, 10)))
+
+#test_Complex <- function() {
+expect_error(Arg(nanotime(1)), "non-numeric argument to function")
