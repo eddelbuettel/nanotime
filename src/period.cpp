503127025e1b6fda432e0b7aa59eee629bd6fcca
Michael Chirico <chiricom@google.com>
2024-10-30 14:55:00 -0700

    inline, also account for R_LEGACY_RCOMPLEX

--- src/period.cpp
+++ src/period.cpp
@@ -11,8 +11,8 @@
 // See comment in <R_Ext/Complex.h> in R>=4.3.0. As of 2024-10-30
 //   clang trunk with -Wmissing-braces requires the double-braced
 //   approach for creating Rcomplex objects.
-Rcomplex makeComplex(double *re, double *im) {
-#if R_VERSION >= R_Version(4, 3, 0)
+inline Rcomplex makeComplex(double *re, double *im) {
+#if R_VERSION >= R_Version(4, 3, 0) && !defined(R_LEGACY_RCOMPLEX)
   return Rcomplex{{*re, *im}};
 #else
   return Rcomplex{*re, *im};
