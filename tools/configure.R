f8ac3d6d6d12007613967a290b4af37fa6489bae
Dirk Eddelbuettel <edd@debian.org>
2026-05-19 07:05:45 -0500

    Tweak tools/configure.R for more exotic settings

--- tools/configure.R
+++ tools/configure.R
@@ -1,13 +1,15 @@
 
 rver <- getRversion()
-if (rver < "4.3.0") {
-    ## for R versions prior to 4.3.0 we need to set the standard
+si <- Sys.info()
+if (rver < "4.3.0" || (si["sysname"] == "Darwin" && si["machine"] == "x86_64")) {
+    ## for R versions prior to 4.3.0 we need to set the standard, and also on
+    ## the r-release-macos-x86_64 machine which is not as C++20-ready as the rest
     cxxstd <- "CXX_STD = CXX17"
 } else {
     ## for R 4.3.0 C++17 is standard so no need to set it
     cxxstd <- "#CXX_STD = CXX17"
 }
-win <- if (Sys.info()[["sysname"]] == "Windows") ".win" else ""
+win <- if (si["sysname"] == "Windows") ".win" else ""
 infile <- file.path("src", paste0("Makevars", win, ".in"))
 outfile <- file.path("src", paste0("Makevars", win))
 lines <- readLines(infile)
